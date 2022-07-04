130e2c967d2cfa56c6f7f4454d9257a6dfca153c
SRI KRISHNA CHARAN DATLA <108675334+charan021190@users.noreply.github.com>
2022-07-04 09:55:54 -0400

    added fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Favourites')
@@ -7,3 +8,5 @@ streamlit.text('🥗 kale, spinach & rocket smoothie')
 streamlit.text(' 🐔 Hard-boiled Free-Range egg')
 streamlit.text('🥑🍞 Avacado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
