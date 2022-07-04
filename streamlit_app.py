e3f4d4da0ebfe2c6b31c427c945753711ec8252d
SRI KRISHNA CHARAN DATLA <108675334+charan021190@users.noreply.github.com>
2022-07-04 09:26:29 -0400

    added and updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & blueberry oatmeal')
-streamlit.text('kale, spinach & rocket smoothie')
-streamlit.text('Hard-boiled Free-Range egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 omega 3 & blueberry oatmeal')
+streamlit.text('🥗 kale, spinach & rocket smoothie')
+streamlit.text(' 🐔 Hard-boiled Free-Range egg')
+streamlit.text('🥑🍞 Avacado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
